61c3e95cf53d7ef958c99839a6fb788cc35a9a0f
Jiadi Yue <jdyue19@gmail.com>
2015-04-09 20:32:26 +0800

    update init.sh tmux.conf

--- init.sh
+++ init.sh
@@ -5,16 +5,10 @@
 # 
 #         USAGE: ./init.sh 
 # 
-#   DESCRIPTION: 
-# 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
-#          BUGS: ---
-#         NOTES: ---
+#   DESCRIPTION: initial os with vimrc tmux bashrc
 #        AUTHOR: Jiadi Yue (Brady), jdyue19@gmail.com
 #  ORGANIZATION: 
 #       CREATED: 04/06/2015 10:49
-#      REVISION:  ---
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
@@ -25,5 +19,5 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 ln -s ${SCRIPTPATH}/vim ~/.vim
 ln -s ${SCRIPTPATH}/vimrc ~/.vimrc
 ln -s ${SCRIPTPATH}/tmux.conf ~/.tmux.conf
-[[ -f ~/.bashrc ]] && { mv ~/.bashrc ~/.bashrc_back }
+[ -e ${HOME}/.bashrc ] && mv ~/.bashrc ~/.bashrc_back
 ln -s ${SCRIPTPATH}/bashrc ~/.bashrc
